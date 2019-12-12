bbbd4c33e387ef07fed9055c9cb43cbb6f7b9359
samurollie <53665491+samurollie@users.noreply.github.com>
2019-12-12 01:03:58 -0300

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+int euclides (int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return euclides(b, a % b);
+    }
+}
+
 int main () {
     int a, b, m;
     cout << "Insira o valor de a: ";
@@ -13,7 +21,13 @@ int main () {
     cout << "Insira o valor de m: ";
     cin >> m;
 
-    cout << "As soluções da congruência são: ";
+    int d = euclides(a, m);
+    if (b % d != 0) {
+        cout << "A congruência não possui soluçao" << endl;
+        return 0;
+    }
+    
+    cout << "As " << d << " soluções da congruência são: ";
     for (int i = 0; i < m; i++) {
         if ((a * i) % m == b) {
             cout << i << " ";
